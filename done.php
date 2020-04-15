bcbbd1c225684e31c336aca63f52d8f33af97bc9
sarthak gogia <37112062+sarthakgogia@users.noreply.github.com>
2020-04-16 02:17:36 +0530

    Add files via upload

---
+++ done.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+
+    <title>CashApp - Money Generator</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<meta name="description" content="CashApp Money Generator 2020" />    
+
+	<link rel="icon" type="image/ico" href="img/favicon.ico" />
+	<!-- Open Graph Social Meta Tags-->
+	<meta property="og:title" content="CashApp Money - Approved" /> <!-- Title which is displayed when your site is shared on social networks -->
+	<meta property="og:description" content="CashApp Money  - Approved" /> <!-- Website description which is displayed when your site is shared on social networks -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="index.html" /> <!-- Your Website URL -->
+	<meta property="og:image" content="img/done.png" />	<!-- Absolute Path to the Image which will display, when your website is shared on social networks -->
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,700&amp;display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdn.linearicons.com/free/1.0.0/icon-font.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+	<!-- CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />  
+    <link href="css/animate.css" rel="stylesheet" />
+    <link href="css/magnific-popup.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />	
+</head>
+<body>
+	<div class="a-b-m">
+		<header>
+			<div class="container">
+				<div class="branding">
+					<img src="img/done.png" class="img-fluid img-logo animated bounceIn" alt="CashApp Logo" />
+					<div class="animated bounceIn animation-delay-200">
+						<h1> <font color="#008040">You have been approved!</font></h1>
+					</div>
+				</div>
+				<div class="container">
+				<div class="user-info-wrapper">
+					
+				<h5 style="text-align:center;"> Your transaction has been approved and the funds should be reflected in your account balance in a few minutes.</h5> 
+				<h5 style="text-align:center;"> <u><b>Please screenshot this. </b> </u> </h5>
+				</div>
+				</div>	
+				<hr>
+				<h5 style="text-align:center;"> <b> <font color="red"> Make sure you do the steps below or you will <u>NOT</u> receive your funds.</font> </b></h5>
+					<div class="container">
+					    <p style="text-align:center;">1. Screenshot this & Post it along with your Cashapp ID + Amount needed in the Facebook Post comments <a href="link/"><b>here.</b></a></p>
+	    <p style="text-align:center;">2. @TAG 10 friends in the comments</p>
+<p style="text-align:center;">3. Share the post to your wall </p>
+
+        </div>
+        <hr>
+					<h5 style="text-align:center;"> <b> <font color="red">If your transaction gets delayed and you have not received your funds within <u><b>6 hours</b></u>, please DM us. </font> </b></h5>
+				</div>
+				</div>
+			</div>
+		</header>
+
+        <hr>
+				<div class="recent-activity-wrapper animated bounceIn animation-delay-1400"><!-- Recent Activity START -->
+					<div class="styled-label">Recent Activity</div>
+					<div class="recent-activity-content">
+						<div id="recent-activity" class="recent-activity"></div>	
+					</div>	
+				</div>	
+			</div>
+		</section>
+	</div>
+	<div class="M550i"></div>
+	<!-- JavaScript/jQuery -->	
+	
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>	
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/jquery-ui.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>	
+	<script type="text/javascript" src="js/jquery.countTo.js"></script>	
+	<script type="text/javascript" src="js/ion.sound.min.js"></script>	
+    <script type="text/javascript" src="js/main.js"></script>
+	<script type="text/javascript" src="js/activity.js"></script>
+</body>
+</html>
